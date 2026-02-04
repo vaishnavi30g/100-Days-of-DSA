854e6dc08ecd75af83dcae75ad506393d6d1ec81
Vaishnavi <vaishnavi30ab@gmail.com>
2026-02-04 14:20:00 +0530

    Qs1.Reversing.c

---
+++ Day_4/Qs1.Reversing.c
@@ -0,0 +1,44 @@
+/*
+Problem: Given an array of n integers, reverse the array in-place using two-pointer approach.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the reversed array, space-separated
+
+Example:
+Input:
+5
+1 2 3 4 5
+
+Output:
+5 4 3 2 1
+
+Explanation: Swap pairs from both ends: (1,5), (2,4), middle 3 stays
+*/
+#include<stdio.h>
+
+int main()
+{
+    int n,temp;
+    printf("Enter the number of elements for an array\n");
+    scanf("%d",&n);
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        printf("Enter the element\n");
+        scanf("%d",&arr[i]);
+    }
+    for (int j = 0; j < n / 2; j++) {
+            temp = arr[j];
+            arr[j] = arr[n - 1 - j];
+            arr[n - 1 - j] = temp;
+        }
+    printf("Updated array: ");
+    for (int j = 0; j < n; j++) {
+        printf("%d ", arr[j]);
+    }  
+    return 0;
+}

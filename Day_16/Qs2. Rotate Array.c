be5b0ef9b03d858b48df002498e83d80f7ef7927
Vaishnavi <vaishnavi30ab@gmail.com>
2026-02-16 19:52:37 +0530

    Create Qs2. Rotate Array.c

---
+++ Day_16/Qs2. Rotate Array.c
@@ -0,0 +1,21 @@
+/*Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.*/
+
+void reverse(int* nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+    if (numsSize <= 1) return;
+
+    k = k % numsSize;
+
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}

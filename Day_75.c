7414a3384226485b2dca320abda4b99da0779950
Vaishnavi <vaishnavi30ab@gmail.com>
2026-04-19 23:53:34 +0530

    Day_75.c

---
+++ Day_75.c
@@ -0,0 +1,35 @@
+/*Problem Statement
+Given an array of integers containing both positive and negative values, find the length of the longest contiguous subarray whose sum is equal to zero.
+
+Input Format
+An integer array arr[].
+
+Output Format
+Print the length of the longest subarray with sum equal to zero.
+
+Sample Input
+15 -2 2 -8 1 7 10 23
+
+Sample Output
+5
+
+Explanation
+The subarray [-2, 2, -8, 1, 7] has a sum of 0 and is the longest such subarray.*/
+
+Problem Statement
+Given an array of integers containing both positive and negative values, find the length of the longest contiguous subarray whose sum is equal to zero.
+
+Input Format
+An integer array arr[].
+
+Output Format
+Print the length of the longest subarray with sum equal to zero.
+
+Sample Input
+15 -2 2 -8 1 7 10 23
+
+Sample Output
+5
+
+Explanation
+The subarray [-2, 2, -8, 1, 7] has a sum of 0 and is the longest such subarray.
